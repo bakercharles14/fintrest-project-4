d705f741fdb78012e6577b815482d4c36c7d2360
Charles Baker <bakercharles14@gmail.com>
2020-06-10 13:52:52 -0400

    added views for models/serializers

--- fintrest/views.py
+++ fintrest/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import CreatorSerializer, PostSerializer, FinSerializer, AllPostsSerializer
+from .models import Creator, Post, Fin
+
+
+class CreatorView(viewsets.ModelViewSet):
+    queryset = Creator.objects.all()
+    serializer_class = CreatorSerializer
+
+
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class FinView(viewsets.ModelViewSet):
+    queryset = Fin.objects.all()
+    serializer_class = FinSerializer
+
+
+class AllPostsView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = AllPostsSerializer
