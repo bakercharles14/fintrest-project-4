729cfcd661df1eee72c983232edcb51566ef78a1
Charles Baker <bakercharles14@gmail.com>
2020-06-10 15:52:15 -0400

    removed package json from root

--- fintrest_project/urls.py
+++ fintrest_project/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.urls import path, include, re_path
 from django.contrib import admin
 from . import views
-from django.urls import path, include, re_path
-from django.contrib import admin
-from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.FrontendAppView.as_view()), #New URL for the index route
+    path('', views.FrontendAppView.as_view()),  # New URL for the index route
     path('api/v1/', include('fintrest.urls')),
-    re_path(r'^.*$', views.FrontendAppView.as_view()), #Says search Frontend for all other routes, so refreshing doesn't make the deployed page error
+    # Says search Frontend for all other routes, so refreshing doesn't make the deployed page error
+    re_path(r'^.*$', views.FrontendAppView.as_view()),
 ]
