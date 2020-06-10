891f04c4e6d74ae5259d0484b46329098a926ced
Charles Baker <bakercharles14@gmail.com>
2020-06-10 13:53:12 -0400

    added router to urls for views

--- fintrest/urls.py
+++ fintrest/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('creator', views.CreatorView)
+router.register('post', views.PostView)
+router.register('fin', views.FinView)
+router.register('allposts', views.AllPostsView)
+
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
