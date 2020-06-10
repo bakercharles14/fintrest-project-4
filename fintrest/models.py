8a80c61cfb8c1152defe9cd70d3a0889c6122271
Charles Baker <bakercharles14@gmail.com>
2020-06-10 13:52:23 -0400

    created models for creator, post, and fin

--- fintrest/models.py
+++ fintrest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+    image = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField(auto_now=True)
+    content = models.TextField()
+    image = models.TextField()
+    creator = models.ForeignKey(
+        Creator, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+
+class Fin(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='fins')
+    creator = models.ForeignKey(
+        Creator, on_delete=models.CASCADE, related_name='fins')
