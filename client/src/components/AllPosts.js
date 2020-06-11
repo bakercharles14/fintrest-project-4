101da0baae7a5175ca879e2402ae07e9f09e3e0f
Charles Baker <bakercharles14@gmail.com>
2020-06-11 10:11:43 -0400

    added content to allposts js

--- client/src/components/AllPosts.js
+++ client/src/components/AllPosts.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class AllPosts extends React.Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        this.getAllPosts()
+    }
+
+    getAllPosts = async () => {
+        try {
+            const res = await axios.get('/api/v1/allposts/')
+            const newState = { ...this.state }
+            newState.posts = res.data
+            this.setState(newState)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     render() {
         return (
             <div>
                 <h1>All Posts</h1>
+                {this.state.posts.map((post) => {
+                    return (
+                        <Link to={`/post/${post.id}`}>
+                            <div>
+                                <div>{post.title}</div>
+                                <div>{post.date}</div>
+                                <div>{post.content}</div>
+                                <img alt={post.title} src={post.image} />
+                                <div>{post.creator}</div>
+                            </div>
+                        </Link>)
+                })}
             </div>
         )
     }
