a292419e6f04f40c7ce1ec48ba2c38193c9a8f21
Charles Baker <bakercharles14@gmail.com>
2020-06-11 09:25:00 -0400

    Created components for app

---
+++ client/src/components/AllPosts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class AllPosts extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>All Posts</h1>
+            </div>
+        )
+    }
+}
---
+++ client/src/components/Creator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Creator extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Creators</h1>
+            </div>
+        )
+    }
+}
